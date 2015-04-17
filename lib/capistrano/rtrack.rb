38cd16e0065d5399ce75f762b1ad2902234caaea
glebtv <glebtv@gmail.com>
2015-04-17 11:34:25 +0300

    use https

--- lib/capistrano/rtrack.rb
+++ lib/capistrano/rtrack.rb
@@ -6,7 +6,7 @@ require 'json'
 module Capistrano
   module Rtrack
     def self.run
-      conn = Faraday.new(url: "http://rtrack.ru") do |faraday|
+      conn = Faraday.new(url: "https://rtrack.ru") do |faraday|
         faraday.request :url_encoded
         faraday.adapter  Faraday.default_adapter 
       end
